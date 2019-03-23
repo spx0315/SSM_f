010f02cc903f922bb7ef83b4f479ecc82ab673a7
spx0315 <1445024781@qq.com>
2019-03-23 18:19:23 +0800

    添加service,pojo,dao,controller

--- src/main/java/com/spx0315/controller/UserController.java
+++ src/main/java/com/spx0315/controller/UserController.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    @Autowired
+    @Resource
     private IUserService userService;
 
     @RequestMapping(value = "findAll",method = RequestMethod.GET)
