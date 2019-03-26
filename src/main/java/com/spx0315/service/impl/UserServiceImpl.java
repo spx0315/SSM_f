01e0654e0a37684dc34787ea38e5cd14285bd0bd
spx0315 <1445024781@qq.com>
2019-03-26 19:01:50 +0800

    Thanks my dear Bro.Yuan 结果是路径名打错了

--- src/main/java/com/spx0315/service/impl/UserServiceImpl.java
+++ src/main/java/com/spx0315/service/impl/UserServiceImpl.java
@@ -6,13 +6,14 @@ import com.spx0315.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Service("IUser")
 public class UserServiceImpl implements IUserService {
 
     /*调用dao层*/
-    @Autowired
+    @Resource
     private IUserDao iUserDao;
     @Override
     public List<User> getUser() {
